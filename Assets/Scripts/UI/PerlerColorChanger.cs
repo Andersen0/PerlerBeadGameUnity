f2e9eb0533ffd6046a0ffbda5624c6678ed5ab7a
Elias Evjen Hartmark <elias.hartmark@gmail.com>
2025-04-12 20:09:14 +0200

    Add instant generation of slider elements to Unity

--- Assets/Scripts/UI/PerlerColorChanger.cs
+++ Assets/Scripts/UI/PerlerColorChanger.cs
@@ -4,40 +4,71 @@ using UnityEngine.UI; // Required when Using UI elements.
 
 public class PerlerColorChanger : MonoBehaviour
 {
-    public Slider redSlider;
-    public Slider greenSlider;
-    public Slider blueSlider;
+    public Slider redSliderInfo;
+    public Slider greenSliderInfo;
+    public Slider blueSliderInfo;
 
     public static Color SelectedColor = Color.white;
 
     void Start()
     {
+        // UI Canvas
+        GameObject canvasUI = new GameObject("Canvas", typeof(Canvas));
+        Canvas canvas = canvasUI.GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay; // HUD Placement
 
-        redSlider = GameObject.Find("redSlider").GetComponent<Slider>();
-        greenSlider = GameObject.Find("greenSlider").GetComponent<Slider>();
-        blueSlider = GameObject.Find("blueSlider").GetComponent<Slider>();
+        canvasUI.AddComponent<CanvasScaler>(); // resolution scaling
+        canvasUI.AddComponent<GraphicRaycaster>(); // allows interaction
+
+        // Create the Slider GameObject
+        GameObject redSlider = new GameObject("Slider", typeof(Slider), typeof(RectTransform));
+        GameObject greenSlider = new GameObject("Slider", typeof(Slider), typeof(RectTransform));
+        GameObject blueSlider = new GameObject("Slider", typeof(Slider), typeof(RectTransform));
+
+        redSlider.transform.SetParent(canvasUI.transform, false);
+        greenSlider.transform.SetParent(canvasUI.transform, false);
+        blueSlider.transform.SetParent(canvasUI.transform, false);
+
+        // Set slider position/size (example)
+        RectTransform top = redSlider.GetComponent<RectTransform>();
+        RectTransform middle = greenSlider.GetComponent<RectTransform>();
+        RectTransform bottom = blueSlider.GetComponent<RectTransform>();
+
+        top.sizeDelta = new Vector2(200, 20);
+        top.anchoredPosition = Vector2.zero;
+
+        middle.sizeDelta = new Vector2(180, 20);
+        middle.anchoredPosition = Vector2.zero;
+
+        bottom.sizeDelta = new Vector2(160, 20);
+        bottom.anchoredPosition = Vector2.zero;
+
+
+        redSliderInfo = GameObject.Find("redSlider").GetComponent<Slider>();
+        greenSliderInfo = GameObject.Find("greenSlider").GetComponent<Slider>();
+        blueSliderInfo = GameObject.Find("blueSlider").GetComponent<Slider>();
 
         // Changes the max value of the slider to 20;
-        redSlider.maxValue = 255;
-        greenSlider.maxValue = 255;
-        blueSlider.maxValue = 255;
+        redSliderInfo.maxValue = 255;
+        greenSliderInfo.maxValue = 255;
+        blueSliderInfo.maxValue = 255;
 
-        blueSlider.wholeNumbers = true;
-        greenSlider.wholeNumbers = true;
-        blueSlider.wholeNumbers = true;
+        redSliderInfo.wholeNumbers = true;
+        greenSliderInfo.wholeNumbers = true;
+        blueSliderInfo.wholeNumbers = true;
 
-        redSlider.onValueChanged.AddListener(delegate { UpdateSelectedColor(); });
-        greenSlider.onValueChanged.AddListener(delegate { UpdateSelectedColor(); });
-        blueSlider.onValueChanged.AddListener(delegate { UpdateSelectedColor(); });
+        redSliderInfo.onValueChanged.AddListener(delegate { UpdateSelectedColor(); });
+        greenSliderInfo.onValueChanged.AddListener(delegate { UpdateSelectedColor(); });
+        blueSliderInfo.onValueChanged.AddListener(delegate { UpdateSelectedColor(); });
 
         UpdateSelectedColor(); // Set initial color
     }
 
     void UpdateSelectedColor()
     {
-        float r = redSlider.value / 255f;
-        float g = greenSlider.value / 255f;
-        float b = blueSlider.value / 255f;
+        float r = redSliderInfo.value / 255f;
+        float g = greenSliderInfo.value / 255f;
+        float b = blueSliderInfo.value / 255f;
 
         SelectedColor = new Color(r, g, b);
     }
