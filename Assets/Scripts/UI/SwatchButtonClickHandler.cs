0bac9eec44629d9ebc11cec9166f80eb1cd356d9
Andersen <118444400+Andersen0@users.noreply.github.com>
2025-05-19 12:48:03 +0200

    Reapply "Merge branch 'main' into features-camera-state-machines"
    This reverts commit 48eddb92301792bef0d5bf0caff1c60af20eaca6.

---
+++ Assets/Scripts/UI/SwatchButtonClickHandler.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class SwatchButtonClickHandler : MonoBehaviour, IPointerClickHandler
+{
+    public int index;
+    public ColorSwatchManager manager;
+
+    private float lastClickTime = 0f;
+    private const float doubleClickThreshold = 0.2f;
+    private Coroutine singleClickCoroutine;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        float currentTime = Time.time;
+
+        if (currentTime - lastClickTime <= doubleClickThreshold)
+        {
+            // Double click detected
+            if (singleClickCoroutine != null)
+            {
+                StopCoroutine(singleClickCoroutine); // Cancel pending single click
+                singleClickCoroutine = null;
+            }
+
+            manager.OnDoubleClick(index);
+        }
+        else
+        {
+            // Schedule single click with a delay
+            singleClickCoroutine = StartCoroutine(DelayedSingleClick());
+        }
+
+        lastClickTime = currentTime;
+    }
+
+    private IEnumerator DelayedSingleClick()
+    {
+        yield return new WaitForSeconds(doubleClickThreshold);
+        manager.OnSingleClick(index);
+        singleClickCoroutine = null;
+    }
+}
